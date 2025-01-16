460f4555919892713b0818063813c9e14e72d2e6
Qishuo Yin <qy1448@princeton.edu>
2025-01-16 11:23:55 -0500

    Changes inside a File

--- data_generate_linear.py
+++ data_generate_linear.py
@@ -88,7 +88,7 @@ def write_csv_dataset(p, r, n, sigma_y, simulation):
     for t in range(simulation):
         X, T, Y, pi, mu, tau, B, f, u = generate_dataset(p, r, n, sigma_y)
         dataset = concat_dataset(X, T, Y)
-        path_outer = '/Users/Qishuo/Code/Double_Deep_Learning/data_simulation/data_simulation_linear/'
+        path_outer = '/Users/Qishuo/Code/Double_Deep_Learning/data_simulation/'
         path_inner = 'data_p_' + str(p) + '_sim_' + str(t) + '.csv'
         dataset.to_csv(path_outer + path_inner, index=False)
         print('p:' + str(p) + '; ' + 'simulation:' + str(t))
