b01bff287264044f2dc79f5d7d41604f7adddcdc
Qishuo Yin <qy1448@princeton.edu>
2025-01-21 00:30:46 -0500

    Update run_DML.sh

--- exp/exp1_method_compare/run_DML.sh
+++ exp/exp1_method_compare/run_DML.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=run_DML_linear     # create a short name for your job
+#SBATCH --job-name=run_DML       # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=4G                 # total memory per node (4 GB per cpu-core is default)
-#SBATCH --time=144:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=144:00:00         # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send mail when job begins
 #SBATCH --mail-type=end          # send mail when job ends
 #SBATCH --mail-type=fail         # send mail if job fails
@@ -12,6 +12,6 @@
 
 module purge
 module load anaconda3/2024.2
-conda activate FAST_NN_ATE_env
+conda activate Double_Deep_Learning_env
 
-python run_DML_linear.py
+python run_DML.py
