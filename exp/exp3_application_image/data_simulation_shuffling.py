6c98b5389c80d4120de6b33f3aacd285bdd6b9ec
Qishuo Yin <qy1448@princeton.edu>
2025-01-29 18:35:17 -0500

    new file:   exp/exp3_application_image/data_simulation_shuffling.py new file:   exp/exp3_application_image/data_simulation_shuffling.sh new file:   exp/exp3_application_image/run_CF_application.py new file:   exp/exp3_application_image/run_CF_application.sh new file:   exp/exp3_application_image/run_DML_application.py new file:   exp/exp3_application_image/run_DML_application.sh new file:   exp/exp3_application_image/run_DR_application.py new file:   exp/exp3_application_image/run_DR_application.sh new file:   exp/exp3_application_image/run_FASTNN_application.py new file:   exp/exp3_application_image/run_FASTNN_application.sh new file:   exp/exp3_application_image/run_Ganite_application.py new file:   exp/exp3_application_image/run_Ganite_application.sh new file:   exp/exp3_application_image/run_VanillaNN_L2_application.py new file:   exp/exp3_application_image/run_VanillaNN_L2_application.sh

---
+++ exp/exp3_application_image/data_simulation_shuffling.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 14:44:50 2025
+
+@author: Qishuo
+
+Double Deep Learning: creat multiple semi-synthetic datasets by shuffling for application
+
+"""
+
+# import packages
+import numpy as np
+import pandas as pd
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+from utility.utility_data import read_dataset_csv_to_numpy
+import os
+
+
+# set relative project path for the project 'Double_Deep_Learning'
+path_file = os.path.dirname(__file__)
+
+
+# load cleaned real dataset for synthetics generation
+path_outer = path_file + '/data_raw/'
+path_inner = 'data_entire.csv'
+path_inner_variable = 'variable_entire.csv'
+dataset_entire = read_dataset_csv_to_numpy(path_outer, path_inner)
+variable_entire = read_dataset_csv_to_numpy(path_outer, path_inner_variable)
+
+
+# set simulation parameters
+n_real = dataset_entire.shape[0]
+simulation = 100
+sigma_y = 0.25 # outcome noice level
+
+
+for t in range(simulation): 
+    index = np.random.choice(range(n_real), size=5000, replace=False)
+    dataset_sim = pd.DataFrame(dataset_entire[index, :])
+    variable_sim = pd.DataFrame(variable_entire[index, :])
+    
+    path_outer_sim = path_file + '/data_simulation/'
+    path_inner_sim = 'data_sim_' + str(t) + '.csv'
+    dataset_sim.to_csv(path_outer_sim + path_inner_sim, index=False)
+    path_inner_variable_sim = 'variable_sim_' + str(t) + '.csv'
+    variable_sim.to_csv(path_outer_sim + path_inner_variable_sim, index=False)
+    print("simulation:" + str(t))
---
+++ exp/exp3_application_image/run_DR_application.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 13:04:07 2024
+
+@author: Qishuo
+
+Double Deep Learning: script to run ATE estimation by Double Robust (DR) estimator for application
+
+"""
+
+# import packages
+import numpy as np
+import pandas as pd
+import torch
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+from utility.utility_data import read_dataset_csv_to_numpy
+from utility.utility_data import data_split_X_T_Y
+from utility.utility_data import variable_split_pi_mu_tau
+from econml.dr import DRLearner
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+import os
+
+
+# set relative project path for the project 'Double_Deep_Learning'
+path_file = os.path.dirname(__file__)
+path_file_parent = os.path.dirname(os.getcwd())
+
+# run script on gpu if possible
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# set seed
+seed = 2024
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+
+# intialize parameter value 
+n = 5000 # syntehtic dataset size
+simulation = 100 # time of simulations
+
+
+# initialize parameter value - training model
+epochs = 100
+batchsize = 64
+learning_rate = 0.0001
+# r = 4
+# r_bar = 10
+L = 4
+N = 300
+
+
+# data and file path
+path_data_outer = path_file + '/data_simulation/'
+path_result_outer = path_file + '/result/'
+path_variable_outer = path_file + '/variable/'
+
+
+# run simulation
+ATE_hat_mat = np.zeros(simulation)
+ATE_ci_low_mat = np.zeros(simulation)
+ATE_ci_up_mat = np.zeros(simulation)
+ATE_true_mat = np.zeros(simulation)
+
+
+coverage_count = 0
+
+for t in range(simulation): 
+    
+    # import data
+    print("simulation:" + str(t))
+    path_outer_sim = path_file + '/data_simulation/'
+    path_inner_sim = 'data_sim_' + str(t) + '.csv'
+    dataset_sim = read_dataset_csv_to_numpy(path_outer_sim, path_inner_sim)
+    path_inner_variable_sim = 'variable_sim_' + str(t) + '.csv'
+    variable_sim = read_dataset_csv_to_numpy(path_outer_sim, path_inner_variable_sim)
+    X, T, Y = data_split_X_T_Y(dataset_sim)
+    pi, mu, tau = variable_split_pi_mu_tau(variable_sim)
+    ATE_true = np.mean(tau)
+    ATE_true_mat[t] = ATE_true
+    
+    # run functions
+    estimator = DRLearner(model_regression=RandomForestRegressor(n_estimators=100, max_depth=50),
+                          model_propensity=RandomForestClassifier(n_estimators=100, max_depth=50),
+                          model_final=RandomForestRegressor(n_estimators=100, max_depth=2),
+                          random_state=seed)
+    estimator.fit(Y=Y, T=T, X=X, W=None)
+    ATE_hat = estimator.ate(X)
+    ITE_hat = estimator.effect(X)
+    ATE_ci_low = np.percentile(ITE_hat, 2.5)
+    ATE_ci_up = np.percentile(ITE_hat, 97.5)
+    
+    # save results & intermediate variables
+    ATE_hat_mat[t] = ATE_hat
+    ATE_ci_low_mat[t] =  ATE_ci_low
+    ATE_ci_up_mat[t] =  ATE_ci_up
+    if (ATE_ci_low <= ATE_true) and (ATE_true <= ATE_ci_up): 
+        coverage_count += 1
+    
+MSE = sum(np.square(ATE_hat_mat - ATE_true_mat)) / simulation
+coverage = coverage_count / simulation
+final_results = np.zeros((1, 2))
+final_results[0, 0] = MSE
+final_results[0, 1] = coverage
+
+  
+# save results
+path_inner_ATE = 'DR_ATE_hat.csv'
+path_inner_ATE_ci_low = 'DR_ATE_ci_low.csv'
+path_inner_ATE_ci_up = 'DR_ATE_ci_up.csv'
+path_inner_final_results = 'DR_final_results.csv'
+pd.DataFrame(ATE_hat_mat).to_csv(path_result_outer + path_inner_ATE, index=False)  
+pd.DataFrame(ATE_ci_low_mat).to_csv(path_result_outer + path_inner_ATE_ci_low, index=False) 
+pd.DataFrame(ATE_ci_up_mat).to_csv(path_result_outer + path_inner_ATE_ci_up, index=False)
+pd.DataFrame(final_results).to_csv(path_result_outer + path_inner_final_results, index=False)
+
