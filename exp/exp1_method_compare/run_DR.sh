022178cbaa7aef764a5200be1f47db91c32a807f
Qishuo Yin <qy1448@princeton.edu>
2025-03-12 12:31:27 -0400

    Update run_DR.sh

--- exp/exp1_method_compare/run_DR.sh
+++ exp/exp1_method_compare/run_DR.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=16G                 # total memory per node (4 GB per cpu-core is default)
-#SBATCH --time=24:00:00         # total run time limit (HH:MM:SS)
+#SBATCH --time=72:00:00         # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send mail when job begins
 #SBATCH --mail-type=end          # send mail when job ends
 #SBATCH --mail-type=fail         # send mail if job fails
