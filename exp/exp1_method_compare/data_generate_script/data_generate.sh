e6e1f5dafe4f9c83bb6dca02d7e120399dad3637
Qishuo Yin <qy1448@princeton.edu>
2025-03-14 14:48:50 -0400

    Update data_generate.sh

--- exp/exp1_method_compare/data_generate_script/data_generate.sh
+++ exp/exp1_method_compare/data_generate_script/data_generate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=data_generate     # create a short name for your job
-#SBATCH --nodes=1                # node count
-#SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=30        # cpu-cores per task (>1 if multithread tasks)
-#SBATCH --mem-per-cpu=16G         # memory per cpu-core (4G is default)
-#SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=begin        # send email when process begins
-#SBATCH --mail-type=fail         # send email if job fails
-#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --job-name=data_generate         # create a short name for your job
+#SBATCH --output=logs/slurm-%A.%a.out    # stdout file
+#SBATCH --error=logs/slurm-%A.%a.err     # stderr file
+#SBATCH --nodes=1                        # node count
+#SBATCH --ntasks=1                       # total number of tasks across all nodes
+#SBATCH --cpus-per-task=30               # cpu-cores per task (>1 if multithread tasks)
+#SBATCH --mem-per-cpu=16G                # memory per cpu-core (4G is default)
+#SBATCH --time=24:00:00                  # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin                # send email when process begins
+#SBATCH --mail-type=fail                 # send email if job fails
+#SBATCH --mail-type=end                  # send email when job ends
 #SBATCH --mail-user=qy1448@princeton.edu
 
 module purge
