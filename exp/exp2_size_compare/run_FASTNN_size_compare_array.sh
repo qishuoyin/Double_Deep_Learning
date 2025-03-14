086db5b91c78e3afe21fdaadddba532c0549dea3
Qishuo Yin <qy1448@princeton.edu>
2025-03-14 13:26:01 -0400

    Update run_FASTNN_size_compare_array.sh

--- exp/exp2_size_compare/run_FASTNN_size_compare_array.sh
+++ exp/exp2_size_compare/run_FASTNN_size_compare_array.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=run_FASTNN_size_compare_array     # create a short name for your job
 #SBATCH --output=slurm-%A.%a.out                      # stdout file
 #SBATCH --error=slurm-%A.%a.err                       # stderr file
-#SBATCH --nodes=1                                     # node count
+#SBATCH --nodes=10                                     # node count
 #SBATCH --ntasks=1                                    # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1                             # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=16G                              # memory per cpu-core (4G is default)
+#SBATCH --cpus-per-task=10                             # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=128G                              # memory per cpu-core (4G is default)
 #SBATCH --gres=gpu:1                                  # number of gpus per node
 #SBATCH --time=72:00:00                               # total run time limit (HH:MM:SS)
-#SBATCH --array=0-4                                   # job array with index values 0, 1, 2, 3, 4
+#SBATCH --array=0-9                                   # job array with index values 0, 1, 2, 3, 4
 #SBATCH --mail-type=all                               # send email on job start, end and fault
 #SBATCH --mail-user=qy1448@princeton.edu
 
