67e7f849877d7adba5c7e77c1fbda4ad23384b3f
Qishuo Yin <qy1448@princeton.edu>
2025-03-14 14:49:54 -0400

    Update run_CF.sh

--- exp/exp1_method_compare/run_CF.sh
+++ exp/exp1_method_compare/run_CF.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=run_CF        # create a short name for your job
-#SBATCH --nodes=1                # node count
-#SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=16G                 # total memory per node (4 GB per cpu-core is default)
-#SBATCH --time=72:00:00         # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=begin        # send mail when job begins
-#SBATCH --mail-type=end          # send mail when job ends
-#SBATCH --mail-type=fail         # send mail if job fails
+#SBATCH --job-name=run_CF                # create a short name for your job
+#SBATCH --output=logs/slurm-%A.%a.out    # stdout file
+#SBATCH --error=logs/slurm-%A.%a.err     # stderr file
+#SBATCH --nodes=1                        # node count
+#SBATCH --ntasks=1                       # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1                # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=16G                        # total memory per node (4 GB per cpu-core is default)
+#SBATCH --time=72:00:00                  # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin                # send mail when job begins
+#SBATCH --mail-type=end                  # send mail when job ends
+#SBATCH --mail-type=fail                 # send mail if job fails
 #SBATCH --mail-user=qy1448@princeton.edu
 
 module purge
