e389488bfe553994212d1f1a400a87e12b6c50bc
Qishuo Yin <qy1448@princeton.edu>
2025-03-14 14:47:25 -0400

    Update data_generate_size_compare.sh

--- exp/exp2_size_compare/data_generate_size_compare.sh
+++ exp/exp2_size_compare/data_generate_size_compare.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=data_generate_size_compare    # create a short name for your job
+#SBATCH --output=logs/slurm-%A.%a.out            # stdout file
+#SBATCH --error=logs/slurm-%A.%a.err             # stderr file
 #SBATCH --nodes=1                                # node count
 #SBATCH --ntasks=1                               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=30                       # cpu-cores per task (>1 if multithread tasks)
+#SBATCH --cpus-per-task=1                        # cpu-cores per task (>1 if multithread tasks)
 #SBATCH --mem-per-cpu=16G                        # memory per cpu-core (4G is default)
 #SBATCH --time=24:00:00                          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin                        # send email when process begins
