a9b1d762e5f31811091324b16ab919652cab2956
Qishuo Yin <qy1448@princeton.edu>
2025-03-13 23:23:27 -0400

    Update run_Ganite_application.sh

--- exp/exp3_application_image/run_Ganite_application.sh
+++ exp/exp3_application_image/run_Ganite_application.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=4G                 # total memory per node (4 GB per cpu-core is default)
 #SBATCH --gres=gpu:1             # number of gpus per node
-#SBATCH --time=72:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send mail when job begins
 #SBATCH --mail-type=end          # send mail when job ends
 #SBATCH --mail-type=fail         # send mail if job fails
